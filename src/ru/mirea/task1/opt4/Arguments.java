8e06994364ad0050ef1121e5c86adc5643c215ed
Игорь <burtsev-igor@mail.ru>
2022-09-17 10:24:18 +0300

    .

--- src/ru/mirea/task1/opt4/Arguments.java
+++ src/ru/mirea/task1/opt4/Arguments.java
@@ -2,6 +2,7 @@ package ru.mirea.task1.opt4;
 
 public class Arguments {
     public static void main(String[] args){
+        System.out.println("Всего " + args.length + " аргументов:");
         for (int i=0; i<args.length; i++) System.out.println("Аргумент " + i + ": " + args[i]);
     }
 }
