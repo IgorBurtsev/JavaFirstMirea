30b40bc3a573ead576d17fa2401d50e40edd2883
Игорь <burtsev-igor@mail.ru>
2022-09-24 10:56:22 +0300

    .

--- src/ru/mirea/task2/opt2/Ball.java
+++ src/ru/mirea/task2/opt2/Ball.java
@@ -1,4 +1,25 @@
 package ru.mirea.task2.opt2;
 
 public class Ball {
+    private String color;
+    private String pattern;
+    private double volume;
+
+    public Ball () {
+        color = "red";
+        pattern = "solid color";
+        volume = 55.8;
+    }
+
+    public Ball(String c, String p) {
+        color = c;
+        pattern = p;
+        volume = 12.4;
+    }
+
+    public String toString() {
+        System.out.println("The ball is " + color + " with a " + pattern + " pattern and it's volume equals " + volume);
+        return null;
+    }
 }
+
---
+++ src/ru/mirea/task2/opt2/TestBall.java
@@ -0,0 +1,10 @@
+package ru.mirea.task2.opt2;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball();
+        Ball b2 = new Ball("blue", "striped");
+        b1.toString();
+        b2.toString();
+    }
+}
